093033a57421d4270e9235b22a04e6ed967d7d29
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-08-11 15:31:12 +0700

    register and login api

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
+import testRouter from "./routes/test.route.js";
 
 const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
+app.use("/api/test", testRouter);
 
 app.listen(8800, () => console.log("Server is running on port 8800"));
