ba0c5525c0ad6285aa5b095179dafa495f13b805
ThienTan <115565563+ttnb2528@users.noreply.github.com>
2024-08-23 21:57:32 +0700

    Chat with Socket.io

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
 import testRouter from "./routes/test.route.js";
 import userRouter from "./routes/user.route.js";
+import chatRouter from "./routes/chat.route.js";
+import messageRouter from "./routes/message.route.js";
 
 const app = express();
 
@@ -17,5 +19,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/test", testRouter);
+app.use("/api/chats", chatRouter);
+app.use("/api/messages", messageRouter);
 
 app.listen(8800, () => console.log("Server is running on port 8800"));
